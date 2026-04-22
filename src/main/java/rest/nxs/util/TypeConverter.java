406b4348308ba1c0c12adb354664f20669a8e87b
meow2fa <mori@veero.de>
2026-04-22 02:13:20 +0200

    updated TypeConverter.java

--- src/main/java/rest/nxs/util/TypeConverter.java
+++ src/main/java/rest/nxs/util/TypeConverter.java
@@ -1,5 +1,8 @@
 package rest.nxs.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TypeConverter {
 
     public static int toInt(Object obj, int def) {
@@ -28,5 +31,27 @@ public class TypeConverter {
 
     public static String toString(Object obj, String def) {
         return obj != null ? obj.toString() : def;
+    }
+
+    public static List<String> toStringList(Object obj) {
+        if (obj instanceof List<?> list) {
+            List<String> result = new ArrayList<>();
+            for (Object o : list) {
+                result.add(String.valueOf(o));
+            }
+            return result;
+        }
+        return new ArrayList<>();
+    }
+
+    public static List<Integer> toIntList(Object obj) {
+        if (obj instanceof List<?> list) {
+            List<Integer> result = new ArrayList<>();
+            for (Object o : list) {
+                result.add(toInt(o, 0));
+            }
+            return result;
+        }
+        return new ArrayList<>();
     }
 }
