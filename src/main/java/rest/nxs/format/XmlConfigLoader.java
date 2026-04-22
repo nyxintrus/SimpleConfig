1dabebeae795fe9dd084293d0e792f5b89b55079
meow2fa <mori@veero.de>
2026-04-22 02:36:03 +0200

    fixed save error

--- src/main/java/rest/nxs/format/XmlConfigLoader.java
+++ src/main/java/rest/nxs/format/XmlConfigLoader.java
@@ -92,6 +92,70 @@ public class XmlConfigLoader implements ConfigLoader {
 
     @Override
     public void save(File file, MemoryConfig config) {
-        throw new UnsupportedOperationException("XML save not implemented");
+        try {
+            var factory = javax.xml.parsers.DocumentBuilderFactory.newInstance();
+            var builder = factory.newDocumentBuilder();
+            var doc = builder.newDocument();
+
+            org.w3c.dom.Element root = doc.createElement("config");
+            doc.appendChild(root);
+
+            writeXml(doc, root, config.getRaw());
+
+            var transformer = javax.xml.transform.TransformerFactory.newInstance().newTransformer();
+            transformer.setOutputProperty(javax.xml.transform.OutputKeys.INDENT, "yes");
+
+            var source = new javax.xml.transform.dom.DOMSource(doc);
+            var result = new javax.xml.transform.stream.StreamResult(file);
+
+            transformer.transform(source, result);
+
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to save XML", e);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private void writeXml(org.w3c.dom.Document doc, org.w3c.dom.Element parent, Object raw) {
+
+        if (raw instanceof Map) {
+
+            Map<String, Object> map = (Map<String, Object>) raw;
+
+            for (Map.Entry<String, Object> entry : map.entrySet()) {
+
+                String key = sanitize(entry.getKey());
+                Object value = entry.getValue();
+
+                if (value instanceof Map) {
+
+                    org.w3c.dom.Element child = doc.createElement(key);
+                    writeXml(doc, child, value);
+                    parent.appendChild(child);
+
+                } else if (value instanceof java.util.List) {
+
+                    for (Object item : (java.util.List<?>) value) {
+                        org.w3c.dom.Element child = doc.createElement(key);
+                        writeXml(doc, child, item);
+                        parent.appendChild(child);
+                    }
+
+                } else {
+
+                    org.w3c.dom.Element child = doc.createElement(key);
+                    child.setTextContent(String.valueOf(value));
+                    parent.appendChild(child);
+                }
+            }
+
+        } else {
+            parent.setTextContent(String.valueOf(raw));
+        }
+    }
+
+    private String sanitize(String key) {
+        if (key == null || key.isBlank()) return "entry";
+        return key.replaceAll("[^a-zA-Z0-9_-]", "_");
     }
 }
